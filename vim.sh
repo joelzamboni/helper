f6accefbbabab318034185f5d723e051ea8792a6
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2014-02-15 14:33:58 -0500

    Script
    Creation of an script

---
+++ vim.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+echo -n "- Basic Setup..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim 
+if [ $? -ne 0 ]
+then
+  echo -e "\033[31merror\033[0m"
+  exit 1
+else
+  echo -e "\033[32mok\033[0m"
+fi
+
+PACKAGES="
+moll/vim-node
+godlygeek/tabular
+pangloss/vim-javascript
+digitaltoad/vim-jade
+kchmck/vim-coffee-script
+wavded/vim-stylus
+groenewege/vim-less
+tpope/vim-surround
+scrooloose/nerdcommenter 
+scrooloose/nerdtree
+tomtom/tlib_vim
+MarcWeber/vim-addon-mw-utils
+garbas/vim-snipmate
+honza/vim-snippets
+scrooloose/snipmate-snippets 
+scrooloose/syntastic
+tpope/vim-fugitive
+"
+echo "- Starting packages clonning:"
+for package in $PACKAGES
+do
+  echo -en "\t$package..."
+  git clone https://github.com/${package}.git ~/.vim/bundle/${package#*/} > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    echo -e "\033[31merror\033[0m"
+  else
+    echo -e "\033[32mok\033[0m"
+  fi
+done
+echo done
+
+echo -n "- Creating VIMRC file..."
+cat > ~/.vimrc <<EOF
+set nocompatible                              " use VIM settings
+set backspace=indent,eol,start                " allow backspace
+set history=1000                              " number of commands in history
+set wrap                                      " dont wrap lines
+set linebreak                                 " wrap lines at convenient points
+set smartindent                               " smart autoindenting when starting a new line
+set shiftwidth=2                              " number of spaces to use for each step of (auto)indent
+set tabstop=2                                 " number of spaces that a Tab in the file counts for
+set expandtab                                 " use spaces to insert a Tab
+set smarttab                                  " insert or delete (backspace) Tabs
+set autoindent                                " copy indent from current line when starting a new line
+call pathogen#infect()                        " call pathogen
+filetype plugin on                            " load file type plugin
+filetype indent on                            " load file indent plugin
+syntax on                                     " enable syntax
+set scrolloff=999                             " cursor will be always in the middle of the window
+set encoding=utf-8                            " sets the character encoding
+colorscheme elflord                           " seting color theme
+set mouse=a                                   " set all mouse modes
+set ttymouse=xterm2                           " mouse compatible term
+set visualbell                                " no beep
+set laststatus=2                              " always statusline
+set statusline=                               " clean the status line
+set statusline+=%-3.3n\                       " buffer number
+set statusline+=%f\                           " filename
+set statusline+=%h%m%r%w                      " status flags
+set statusline+=\[%{strlen(&ft)?&ft:'none'}]  " file type
+set statusline+=%=                            " right align remainder
+set statusline+=0x%-8B                        " character value
+set statusline+=%-14(%l,%c%V%)                " line, character
+set statusline+=%<%P                          " file position
+set ruler                                     " show the line and column number of the cursor position
+set colorcolumn=80                            " create a line on column 80
+set showcmd                                   " show incomplete cmds down the bottom
+set showmode                                  " show current mode down the botto
+set number                                    " show line numbers (set nonumber to remove)
+set list                                      " display tabs and trailing spaces
+set listchars=tab:▷⋅,trail:⋅,nbsp:⋅           " define symbolx for tabs
+set incsearch                                 " find the next match as we type the search
+set hlsearch                                  " hilight searches by default
+set ignorecase
+set wildmode=list:longest                     " make cmdline tab completion similar to bash
+set wildmenu                                  " enable ctrl-n and ctrl-p to scroll thru matches
+set wildignore=*.o,*.obj,*~                   " stuff to ignore when tab completing
+set clipboard=unnamedplus                     " use X11 clipboard, remember use +yy or +p
+" Customization to modules
+" open NERDTree with <ne>
+nmap ne :NERDTree<cr>
+" improved split navegation Ctrl+key, instead of Ctrl+w+key
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+set makeprg=/usr/local/bin/node\ %            " set make to node
+set autowrite                                 " automatic write before make
+EOF
+if [ $? -ne 0 ]
+then
+  echo -e "\033[31merror\033[0m"
+  exit 1
+else
+  echo -e "\033[32mok\033[0m"
+fi
