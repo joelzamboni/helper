abe71cce69f367c9726efbb01ba1672ed2cccf9b
joelzamboni <joel.zamboni@gmail.com>
2014-04-20 23:46:32 -0400

    add git.sh

---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This is just a reminder, no error check
+
+echo -n '- GIT Username: '
+read username
+echo -n '- GIT Email; '
+read email
+
+git config --global user.name username
+git config --global user.email email
+git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
