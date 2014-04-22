7c068355fec3123779ba67e01f021713f4bac8ff
joelzamboni <joel.zamboni@gmail.com>
2014-04-22 02:06:04 +0000

    fix git.sh

--- git.sh
+++ git.sh
@@ -7,7 +7,7 @@ read username
 echo -n '- GIT Email: '
 read email
 
-git config --global user.name username
-git config --global user.email email
+git config --global user.name $username
+git config --global user.email $email
 git config --global credential.helper 'cache --timeout=3600'
 git config --global push.default simple
