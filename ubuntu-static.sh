b53905fcaed0b2a6a194a3e9e1dd382819affbdb
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2014-04-27 18:42:49 -0400

    new ubuntu-static(not tested)

---
+++ ubuntu-static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Just to configure static IP on ubuntu
+
+echo -n 'IP Address: '
+read ip
+echo -n 'Netmask: '
+read netmask
+echo -n 'Gateway: '
+read gw
+
+cat << EOF | sudo -u root  tee /etc/network/interfaces
+# /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+address ${ip}
+netmask ${netmask}
+gateway ${gw}
+dns-nameserver 8.8.8.8 8.8.4.4
+EOF
+
