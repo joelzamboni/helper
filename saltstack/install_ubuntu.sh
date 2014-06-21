611e3e323de682e0e04c09ee25c3055ebf5c69ae
joelzamboni <joel.zamboni@gmail.com>
2014-06-21 18:45:34 +0000

    sync

---
+++ saltstack/install_ubuntu.sh
@@ -0,0 +1,9 @@
+
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:saltstack/salt
+sudo apt-get update
+sudo apt-get install salt-master
+sudo apt-get install salt-minion
+sudo apt-get install salt-syndic
+
