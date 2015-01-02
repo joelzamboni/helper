902cc51393a59b15290153f41cc9b32fd2360054
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2015-01-02 08:36:30 -0500

    Master and Minion

--- admin/saltstack/install_ubuntu.sh
+++ admin/saltstack/install_ubuntu.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-get install python-software-properties
+
+# Master
 sudo add-apt-repository ppa:saltstack/salt
 sudo apt-get update
-sudo apt-get install salt-master
-sudo apt-get install salt-minion
-sudo apt-get install salt-syndic
+sudo apt-get install salt-master python-software-properties salt-minion salt-syndic
+
+# Minion
+sudo add-apt-repository ppa:saltstack/salt
+sudo apt-get update
+sudo apt-get install python-software-properties salt-minion
+
 
