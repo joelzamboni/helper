f2b60e75a8afd93f4ccb539e3fa643b0197d97d8
joelzamboni <joel.zamboni@gmail.com>
2015-01-21 18:04:35 +0000

    lxc recreate script

---
+++ ubuntu/scripts/recreate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+
+lxc-stop -n $1
+lxc-destroy -n $1
+lxc-create -n $1 -t ubuntu
+lxc-start -n ldap -d 
+sleep 5
+$(lxc-ls -f | grep RUNNING|grep $1|awk '{ print "ssh-copy-id ubuntu@" $3}') 
+
