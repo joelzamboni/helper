0262ec12dc14e99fb82d79bdc8ef2b4157266acf
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2015-02-17 13:38:09 -0500

    Update recreate.sh

--- ubuntu/scripts/recreate.sh
+++ ubuntu/scripts/recreate.sh
@@ -5,7 +5,7 @@ set -x
 lxc-stop -n $1
 lxc-destroy -n $1
 lxc-create -n $1 -t ubuntu
-lxc-start -n ldap -d 
+lxc-start -n $1 -d 
 sleep 5
 $(lxc-ls -f | grep RUNNING|grep $1|awk '{ print "ssh-copy-id ubuntu@" $3}') 
 
