41bb24d1c32627376d8d314ead3b8d952b04305a
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2015-02-23 08:32:14 -0500

    Update config.sh

--- ubuntu/scripts/samba/config.sh
+++ ubuntu/scripts/samba/config.sh
@@ -1,11 +1,21 @@
 
-
+# Samba Server Network Configuration
 ip=
 netmask=
 gw=
 dns=
+
+# External DNS - included in smb.conf
 dns_forwarder=
+
+# Server hostname and netbios name
 hostname=
+
+# Domain name and kerberos realm
 domainname=
+
+# Short netbios domain name
 netbiosdomain=
+
+# Self - used only on server configuration
 administrator_password=
