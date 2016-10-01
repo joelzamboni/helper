bbb0d2e7ae33a6bba27465d3fa7843c98c565985
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2016-10-01 15:44:10 -0400

    Update openvpn.sh

--- ubuntu/scripts/openvpn/sharedkey/openvpn.sh
+++ ubuntu/scripts/openvpn/sharedkey/openvpn.sh
@@ -1,12 +1,11 @@
 
-
+# Server
 port 1195
 proto udp
 dev tun
 ifconfig 192.168.12.5 192.168.12.6
 route 10.12.0.0 255.255.224.0
 route 10.12.192.0 255.255.240.0
-keepalive 10 120
 comp-lzo
 user nobody
 group nogroup
@@ -19,11 +18,13 @@ secret static.key
 openvpn --genkey --secret static.key
 
 
+# Client
 proto udp
 dev tun
 remote 52.1.105.226 1195
 ifconfig 192.168.12.6 192.168.12.5
 route 10.12.142.0 255.255.254.0
+keepalive 10 120
 resolv-retry infinite
 nobind
 persist-key
