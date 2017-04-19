1eeaf22aadd0e061108a61bd4122c42e8932fbd4
joelzamboni <joel.zamboni@gmail.com>
2017-04-19 14:34:06 +0000

    improving  mongo install

--- data/scripts/mongodb-install.sh
+++ data/scripts/mongodb-install.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
+## DO NOT USE
 
-# Install MongoDB on Ubuntu 
-# From http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+# TODO: create /etc/mongod.conf
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
-sudo apt-get install mongodb-org
-sudo service mongod start
+sudo apt-get install -y mongodb-org
+
+cat > /lib/systemd/system/mongod.service <<EOF
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemctl daemon-reload
+systemctl start mongod
+systemctl enable mongod
+
+
 
 
